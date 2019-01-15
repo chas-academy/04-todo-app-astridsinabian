b437136521711325572095d042aef337dce6bbb3
Astrid <astrid.sinabian@chasacademy.se>
2019-01-15 15:59:31 +0100

    Add update function

--- src/Controllers/TodoController.php
+++ src/Controllers/TodoController.php
@@ -28,6 +28,20 @@ class TodoController extends Controller {
         $body = filter_body(); // gives you the body of the request (the "envelope" contents)
         $todoId = $urlParams['id']; // the id of the todo we're trying to update
         $completed = isset($body['status']) ? 1 : 0; // whether or not the todo has been checked or not
+        $title = $body['title'];
+
+        if($completed = 1) {
+          $completed = true;
+        }
+        if($completed = 0) {
+          $completed = false;
+        }
+
+        $result = TodoItem::updateTodo($todoId, $title, $completed);
+
+        if($result) {
+          $this->redirect('/');
+        }
 
         // TODO: Implement me!
         // This action should update a specific todo item in the todos table using the TodoItem::updateTodo method.
--- src/Models/TodoItem.php
+++ src/Models/TodoItem.php
@@ -18,8 +18,14 @@ class TodoItem extends Model
 
     public static function updateTodo($todoId, $title, $completed = null)
      {
-        // TODO: Implement me!
-        // Update a specific todo
+
+        $query = "UPDATE todos SET title = '$title', completed = '$completed' WHERE id = $todoId";
+        self::$db->query($query);
+        $result = self::$db->execute();
+
+
+        return $result;
+
      }
 
     public static function deleteTodo($todoId)
